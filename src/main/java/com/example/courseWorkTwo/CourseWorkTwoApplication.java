5264e8dbab587d82b7f5c4602f8ebe8ae162f163
Berychc <treewincsgo@gmail.com>
2024-02-17 00:17:40 +0300

    Start Course Work Two

---
+++ src/main/java/com/example/courseWorkTwo/CourseWorkTwoApplication.java
@@ -0,0 +1,13 @@
+package com.example.courseWorkTwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWorkTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseWorkTwoApplication.class, args);
+	}
+
+}
